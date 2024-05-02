c9da69002783b22f001c4a63a1203308807b7e0a
ABZB <abzb13@gmail.com>
2024-05-02 03:13:05 -0400

    Constants
    This holds the global constants and will also hold the arrays of species and stuff

---
+++ my_constants.py
@@ -0,0 +1,35 @@
+
+
+#Global path variables   
+personal_path = ''
+levelup_path = ''
+evolution_path = ''
+model_path = ''
+
+#Global file/game variables
+game = ''
+max_species_index = 0
+personal = []
+levelup = []
+evolution = []
+model = []
+
+#on startup, initialize to empty
+#when load, initialize to full and current to full
+#when search, search in current for speed
+base_species_list =  ["Bulby", "Ivy", "Venu"]
+master_formes_list = ["Bulby", "Ivy", "Venu"]
+model_source_list = ["Bulby", "Ivy", "Venu"]
+
+current_base_species_list =  ["Bulbasaur"]
+current_personal_list = ["Bulbasaur"]
+current_levelup_list = ["Bulbasaur"]
+current_evolution_list = ["Bulbasaur"]
+current_model_source_list = ["Bulbasaur"]
+
+personal_filename_length = 0
+evolution_filename_length = 0
+levelup_filename_length = 0
+model_filename_length = 0
+
+extracted_extension = '.bin'

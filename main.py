df0f38c2551ce4d87beb06d9d014e3bdd165f9ae
ABZB <abzb13@gmail.com>
2024-05-08 18:02:50 -0400

    Implement GUI scaling
    Interface now scales with the window size

--- main.py
+++ main.py
@@ -57,13 +57,14 @@ def pre_check(poke_edit_data):
         print("Evolution source is not an integer!")
         return
 
-    print(model_source_index)
+    #print(model_source_index)
     poke_edit_data= add_new_forme_prelim(poke_edit_data, base_form_index, new_forme_count, model_source_index, personal_source_index, levelup_source_index, evolution_source_index, model_bool.get())
     
     return(poke_edit_data)
 
 root = Tk()
-root.geometry('750x300')
+root.title('Pokemon Forme Insertion')
+root.geometry('950x300')
 
 poke_edit_data = Pokedata()
 
@@ -196,14 +197,21 @@ def update_all_listboxes(poke_edit_data):
     update(poke_edit_data.master_formes_list, levelup_listbox)
     update(poke_edit_data.master_formes_list, personal_listbox)
     update(poke_edit_data.base_species_list, base_species_listbox)
-    
+
+for x in range(5):
+    Grid.rowconfigure(root, x, weight = 1)
+
+for y in range(7):
+    Grid.columnconfigure(root, y, weight = 1)
+
+
 #load/save config
-cfg_load = Button(root, text = 'Load CFG', command = lambda: [load_game_cfg(poke_edit_data), update_all_listboxes(poke_edit_data), set_games_checklist(poke_edit_data.game)], height = 2, width = 12, pady = 5, padx = 7)
-cfg_load.grid(row = 0, column = 0)
+cfg_load = Button(root, text = 'Load CFG', command = lambda: [load_game_cfg(poke_edit_data), update_all_listboxes(poke_edit_data), set_games_checklist(poke_edit_data.game)], height = 2, width = 18, pady = 5, padx = 7)
+cfg_load.grid(row = 0, column = 0, sticky="ew")
 
 
-cfg_save = Button(root, text = 'Save CFG & CSV', command = lambda: save_game_cfg(poke_edit_data, games_temp.get()), height = 2, width = 12, pady = 5, padx = 7)
-cfg_save.grid(row = 1, column = 0)
+cfg_save = Button(root, text = 'Save CFG & CSV', command = lambda: save_game_cfg(poke_edit_data, games_temp.get()), height = 2, width = 18, pady = 5, padx = 7)
+cfg_save.grid(row = 1, column = 0, sticky="ew")
 
 
 #select game
@@ -212,36 +220,36 @@ games = ["XY", "ORAS", "SM", "USUM"]
 games_temp = StringVar(root)
 games_temp.set("Select Game")
 game_select = OptionMenu(root, games_temp, *games)
-game_select.grid(row = 0, column = 1)
+game_select.grid(row = 0, column = 1, sticky="ew")
 
 
 #load Model
-model_load = Button(root, text = 'Select Model Path', command = lambda: [choose_GARC(poke_edit_data, "Model", games_temp.get()), update(poke_edit_data.model_source_list, model_listbox)], height = 2, width = 15, pady = 5, padx = 7)
-model_load.grid(row = 0, column = 2)
+model_load = Button(root, text = 'Select Model Path', command = lambda: [choose_GARC(poke_edit_data, "Model", games_temp.get()), update(poke_edit_data.model_source_list, model_listbox)], height = 2, width = 18, pady = 5, padx = 7)
+model_load.grid(row = 0, column = 2, sticky="ew")
 
 #load Personal
-personal_load = Button(root, text = 'Select Personal Path', command = lambda: [choose_GARC(poke_edit_data, "Personal", games_temp.get()), update(poke_edit_data.base_species_list, base_species_listbox), update(poke_edit_data.master_formes_list, personal_listbox), update(poke_edit_data.master_formes_list, levelup_listbox), update(poke_edit_data.master_formes_list, evolution_listbox), update(poke_edit_data.model_source_list, model_listbox)], height = 2, width = 15, pady = 5, padx = 7)
-personal_load.grid(row = 0, column = 3)
+personal_load = Button(root, text = 'Select Personal Path', command = lambda: [choose_GARC(poke_edit_data, "Personal", games_temp.get()), update(poke_edit_data.base_species_list, base_species_listbox), update(poke_edit_data.master_formes_list, personal_listbox), update(poke_edit_data.master_formes_list, levelup_listbox), update(poke_edit_data.master_formes_list, evolution_listbox), update(poke_edit_data.model_source_list, model_listbox)], height = 2, width = 18, pady = 5, padx = 7)
+personal_load.grid(row = 0, column = 3, sticky="ew")
 
 #load Levelup
-levelup_load = Button(root, text = 'Select Levelup Path', command = lambda: choose_GARC(poke_edit_data, "Levelup", games_temp.get()), height = 2, width = 15, pady = 5, padx = 7)
-levelup_load.grid(row = 0, column = 4)
+levelup_load = Button(root, text = 'Select Levelup Path', command = lambda: choose_GARC(poke_edit_data, "Levelup", games_temp.get()), height = 2, width = 18, pady = 5, padx = 7)
+levelup_load.grid(row = 0, column = 4, sticky="ew")
 
 #load Evolution
-evolution_load = Button(root, text = 'Select Evolution Path', command = lambda: choose_GARC(poke_edit_data, "Evolution", games_temp.get()), height = 2, width = 15, pady = 5, padx = 7)
-evolution_load.grid(row = 0, column = 5)
+evolution_load = Button(root, text = 'Select Evolution Path', command = lambda: choose_GARC(poke_edit_data, "Evolution", games_temp.get()), height = 2, width = 18, pady = 5, padx = 7)
+evolution_load.grid(row = 0, column = 5, sticky="ew")
 
 
 
 #Base Species Selection
 base_species_label = Label(root, text = "Select Species", width = 12, padx = 7)
-base_species_label.grid(row = 2, column = 0)
+base_species_label.grid(row = 2, column = 0, sticky="nsew")
 
-base_species_entry = Entry(root, width = 15)
-base_species_entry.grid(row = 3, column = 0)
+base_species_entry = Entry(root, width = 18)
+base_species_entry.grid(row = 3, column = 0, sticky="nsew")
 
-base_species_listbox = Listbox(root, width = 15)
-base_species_listbox.grid(row = 4, column = 0)
+base_species_listbox = Listbox(root, width = 18)
+base_species_listbox.grid(row = 4, column = 0, sticky="nsew")
 
 update(poke_edit_data.base_species_list, base_species_listbox)
 
@@ -252,17 +260,17 @@ base_species_entry.bind("<KeyRelease>", check_base_species)
 #Model Selection
 
 model_checkbutton = Checkbutton(root, text = 'Same as Species', variable = model_bool, onvalue = True, offvalue = False)
-model_checkbutton.grid(row = 1, column = 2)
+model_checkbutton.grid(row = 1, column = 2, sticky="nsew")
 model_checkbutton.select()
 
 model_label = Label(root, text = "Custom Model", width = 12, padx = 3)
-model_label.grid(row = 2, column = 2)
+model_label.grid(row = 2, column = 2, sticky="nsew")
 
-model_entry = Entry(root, width = 15)
-model_entry.grid(row = 3, column = 2)
+model_entry = Entry(root, width = 18)
+model_entry.grid(row = 3, column = 2, sticky="nsew")
 
-model_listbox = Listbox(root, width = 15)
-model_listbox.grid(row = 4, column = 2)
+model_listbox = Listbox(root, width = 18)
+model_listbox.grid(row = 4, column = 2, sticky="nsew")
 
 update(poke_edit_data.model_source_list, model_listbox)
 
@@ -273,17 +281,17 @@ model_entry.bind("<KeyRelease>", check_model)
 #Personal Selection
 
 personal_checkbutton = Checkbutton(root, text = 'Same as Species', variable = personal_bool, onvalue = True, offvalue = False)
-personal_checkbutton.grid(row = 1, column = 3)
+personal_checkbutton.grid(row = 1, column = 3, sticky="nsew")
 personal_checkbutton.select()
 
 personal_label = Label(root, text = "Custom Personal", width = 12, padx = 3)
-personal_label.grid(row = 2, column = 3)
+personal_label.grid(row = 2, column = 3, sticky="nsew")
 
-personal_entry = Entry(root, width = 15)
-personal_entry.grid(row = 3, column = 3)
+personal_entry = Entry(root, width = 18)
+personal_entry.grid(row = 3, column = 3, sticky="nsew")
 
-personal_listbox = Listbox(root, width = 15)
-personal_listbox.grid(row = 4, column = 3)
+personal_listbox = Listbox(root, width = 18)
+personal_listbox.grid(row = 4, column = 3, sticky="nsew")
 
 update(poke_edit_data.master_formes_list, personal_listbox)
 
@@ -294,17 +302,17 @@ personal_entry.bind("<KeyRelease>", check_personal)
 #Levelup Selection
 
 levelup_checkbutton = Checkbutton(root, text = 'Same as Species', variable = levelup_bool, onvalue = True, offvalue = False)
-levelup_checkbutton.grid(row = 1, column = 4)
+levelup_checkbutton.grid(row = 1, column = 4, sticky="nsew")
 levelup_checkbutton.select()
 
 levelup_label = Label(root, text = "Custom Levelup", width = 12, padx = 3)
-levelup_label.grid(row = 2, column = 4)
+levelup_label.grid(row = 2, column = 4, sticky="nsew")
 
-levelup_entry = Entry(root, width = 15)
-levelup_entry.grid(row = 3, column = 4)
+levelup_entry = Entry(root, width = 18)
+levelup_entry.grid(row = 3, column = 4, sticky="nsew")
 
-levelup_listbox = Listbox(root, width = 15)
-levelup_listbox.grid(row = 4, column = 4)
+levelup_listbox = Listbox(root, width = 18)
+levelup_listbox.grid(row = 4, column = 4, sticky="nsew")
 
 update(poke_edit_data.master_formes_list, levelup_listbox)
 
@@ -314,50 +322,47 @@ levelup_entry.bind("<KeyRelease>", check_levelup)
 #Evolution Selection
 
 evolution_checkbutton = Checkbutton(root, text = 'Same as Species', variable = evolution_bool, onvalue = True, offvalue = False)
-evolution_checkbutton.grid(row = 1, column = 5)
+evolution_checkbutton.grid(row = 1, column = 5, sticky="nsew")
 evolution_checkbutton.select()
 
 evolution_label = Label(root, text = "Custom Evolution", width = 12, padx = 4)
-evolution_label.grid(row = 2, column = 5)
+evolution_label.grid(row = 2, column = 5, sticky="nsew")
 
-evolution_entry = Entry(root, width = 15)
-evolution_entry.grid(row = 3, column = 5)
+evolution_entry = Entry(root, width = 18)
+evolution_entry.grid(row = 3, column = 5, sticky="nsew")
 
-evolution_listbox = Listbox(root, width = 15)
-evolution_listbox.grid(row = 4, column = 5)
+evolution_listbox = Listbox(root, width = 18)
+evolution_listbox.grid(row = 4, column = 5, sticky="nsew")
 
 update(poke_edit_data.master_formes_list, evolution_listbox)
 
 evolution_listbox.bind("<Double-1>", fillout_evolution)
 evolution_entry.bind("<KeyRelease>", check_evolution)
 
-#Model list file path Selection
+#CSV file path Selection
 
-load_pokelist_csv_button = Button(root, text = 'Load CSV', command = lambda: user_prompt_load_CSV(poke_edit_data, "Pokemon Names and Files"), height = 2, width = 15, pady = 5, padx = 7)
-load_pokelist_csv_button.grid(row = 0, column = 6)
+load_pokelist_csv_button = Button(root, text = 'Load CSV', command = lambda: user_prompt_load_CSV(poke_edit_data, "Pokemon Names and Files", games_temp.get()), height = 2, width = 18, pady = 5, padx = 7)
+load_pokelist_csv_button.grid(row = 0, column = 6, sticky="ew")
 
-save_pokelist_csv_button = Button(root, text = 'Save CSV', command = lambda: user_prompt_write_CSV(poke_edit_data, "Pokemon Names and Files"), height = 2, width = 15, pady = 5, padx = 7)
-save_pokelist_csv_button.grid(row = 0, column = 6)
+save_pokelist_csv_button = Button(root, text = 'Save CSV', command = lambda: user_prompt_write_CSV(poke_edit_data, "Pokemon Names and Files"), height = 2, width = 18, pady = 5, padx = 7)
+save_pokelist_csv_button.grid(row = 1, column = 6, sticky="ew")
 
-create_pokelist_csv_button = Button(root, text = 'Create/Reset CSV', command = lambda: create_refresh_CSV(poke_edit_data, "Pokemon Names and Files", games_temp.get()), height = 2, width = 15, pady = 5, padx = 7)
-create_pokelist_csv_button.grid(row = 4, column = 6)
+create_pokelist_csv_button = Button(root, text = 'Create/Reset CSV', command = lambda: create_refresh_CSV(poke_edit_data, "Pokemon Names and Files", games_temp.get()), height = 2, width = 18, pady = 5, padx = 7)
+create_pokelist_csv_button.grid(row = 4, column = 6, sticky="ew")
 
 
 #Number of New Formes
-
-
 number_formes_label = Label(root, text = "# Formes To Add", width = 12, padx = 4)
-number_formes_label.grid(row = 2, column = 1)
+number_formes_label.grid(row = 2, column = 1, sticky="nsew")
 
-number_formes_entry = Entry(root, width = 15)
+number_formes_entry = Entry(root, width = 12)
 number_formes_entry.grid(row = 3, column = 1)
 
-#Run
-
-execute_buttom = Button(root, text = 'Insert Forme(s)', command = lambda: [pre_check(poke_edit_data), update_all_listboxes(poke_edit_data)], height = 2, width = 15, pady = 5, padx = 7)
-execute_buttom.grid(row = 4, column = 1)
-
+#Run Insertion
+execute_button = Button(root, text = 'Insert Forme(s)', command = lambda: [pre_check(poke_edit_data), update_all_listboxes(poke_edit_data)], height = 2, width = 12, pady = 5, padx = 7)
+execute_button.grid(row = 4, column = 1, sticky="ew")
 
 
 
+#print("help")
 root.mainloop()

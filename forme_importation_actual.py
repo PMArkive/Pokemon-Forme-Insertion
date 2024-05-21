af236fb4ca4c743d478de023c0d1111da7b535e1
ABZB <abzb13@gmail.com>
2024-05-21 16:04:16 -0400

    rewrite of insertion code
    no longer need to search for open space or rewrite pointers, as that is handled by the new resorting function

--- forme_importation_actual.py
+++ forme_importation_actual.py
@@ -1,97 +1,79 @@
 import shutil
 import os
 import mmap
-import sys
-import errno
+from tkinter.filedialog import asksaveasfile
 from my_constants import *
 from file_handling import *
 
 #handles actual editing and moving of files for forme insertion
-def add_new_forme_execute(poke_edit_data, base_form_index, start_location, new_forme_count, model_source_index, personal_source_index, levelup_source_index , evolution_source_index, existing_formes_array, def_model, total_alt_formes, enough_room):
+def add_new_forme_execute(poke_edit_data, base_form_index, new_forme_count, model_source_index, personal_source_index, levelup_source_index , evolution_source_index, def_model):
     
-    total_formes = total_alt_formes + 1
-    print("Initializing new data")
-    #Part 1, Personal file, update existing Personal blocks with new forme count and pointer
-    personal_file_update(poke_edit_data, base_form_index, total_formes, start_location)
     
-    for pokemon in existing_formes_array:
-        personal_file_update(poke_edit_data, pokemon, total_formes, start_location)
-        
-    #for Levelup, evo, and Personal, move existing formes if needed. We need to delete existing files if they are there, then copy them to the new location, then overwrite the old spot with zeroes
-    #offset starts from 0, which is the first forme that is at index start_location
-    if(not(enough_room)):
-        for offset, pokemon in enumerate(existing_formes_array):
-    
-            #delete target spots if they exist (only happens if filling a spot cleared by moving something else)
-            silentremove(file_namer(poke_edit_data.personal_path, start_location + offset, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension))
-            silentremove(file_namer(poke_edit_data.evolution_path, start_location + offset, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension))
-            silentremove(file_namer(poke_edit_data.levelup_path, start_location + offset, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension))
 
-            #copy personal file to new location
-            shutil.copy(file_namer(poke_edit_data.personal_path, pokemon, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.personal_path, start_location + offset, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension))
-        
-            #zero out old personal block
-            with open(file_namer(poke_edit_data.personal_path, pokemon, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), "r+b") as f:
-                with mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE) as personal_hex_map:
-                    personal_hex_map.flush()
-                    personal_hex_map.resize(poke_edit_data.personal_table_length)
-                    for x in range(0, poke_edit_data.personal_table_length):
-                        personal_hex_map[x] = 0x00
-                    personal_hex_map.flush()
-        
-            #copy evolution file to new location
-            shutil.copy(file_namer(poke_edit_data.evolution_path, pokemon, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.evolution_path, start_location + offset, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension))
-        
-            #zero out old evolution block
-            with open(file_namer(poke_edit_data.evolution_path, pokemon, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension), "r+b") as f:
-                with mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE) as evolution_hex_map:
-                    evolution_hex_map.flush()
+    #first unused file name
+    start_location = 0
+    
+    #checks to ensure that the evolution and levelup folders have the same number of files, and that the personal folder has either 1 more (because compilation file) or the same (if it was removed)
+    if(poke_edit_data.evolution[-1]  ==  poke_edit_data.levelup[-1] and (poke_edit_data.levelup[-1] in {poke_edit_data.personal[-1], poke_edit_data.personal[-2]})):
+        start_location = poke_edit_data.evolution[-1] + 1
+    else:
+        print('Mismatch in file counts:\n')
+        print('Personal has:', poke_edit_data.personal[-1], 'files')
+        print('Levelup has:', poke_edit_data.levelup[-1], 'files')
+        print('Evolution has:', poke_edit_data.evolution[-1], 'files')
+        return(poke_edit_data)
+    
+    #Grab any existing formes
+    existing_formes_array = []
 
-                    for x in range(0, poke_edit_data.evolution_table_length):
-                        evolution_hex_map[x] = 0x00
-                    evolution_hex_map.flush()
-        
-            #copy levelup file to new location
-            shutil.copy(file_namer(poke_edit_data.levelup_path, pokemon, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.levelup_path, start_location + offset, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension))
-        
-            #write Tackle at Level 1 to old location (avoid pk3ds crash)
-            with open(file_namer(poke_edit_data.levelup_path, pokemon, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension), "r+b") as f:
-                with mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE) as levelup_hex_map:
-                    levelup_hex_map.flush()
-                    levelup_hex_map.resize(0x08)
-                    levelup_hex_map[0] = 0xED
-                    levelup_hex_map[1] = 0x00
-                    levelup_hex_map[2] = 0x01
-                    levelup_hex_map[3] = 0x00
-                    for x in range(4,0x08):
-                        levelup_hex_map[x] = 0xFF
-                    levelup_hex_map.flush()
-                
+    total_formes = new_forme_count
+    
+    print('Updating existing files for species ' + str(base_form_index))
+    with open(file_namer(poke_edit_data.personal_path, base_form_index, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), "r+b") as f:
+        with mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE) as personal_hex_map:
+            
+            #Get the new total number of formes
+            total_formes += personal_hex_map[0x20]
+            
+            #find existing formes
+            if(personal_hex_map[0x20] > 0x01):
+                for offset in range(0, personal_hex_map[0x20] - 1):
+                    temp_pointer = personal_hex_map[0x1C] + personal_hex_map[0x1D]*256 + offset
+                    
+                    #save current forme file pointer to the formes array
+                    existing_formes_array.append(temp_pointer)
+                    
+                    #set new number of total formes. Don't bother resetting the internal forme pointer because that will be handled by the sort at the end
+                    personal_file_update(poke_edit_data, temp_pointer, total_formes, -1)
+            
+            #set new number of formes in this personal file as well before we exit
+            personal_hex_map[0x20] = total_formes
+                    
+    
+    print("Initializing new game data")
     #now initialize the newly added formes
-    
-    #offset to adjust for preexisting formes
-    new_offset_start = len(existing_formes_array)
+
+    #delete the old compilation file if it's there
+    if(os.path.getsize(file_namer(poke_edit_data.personal_path, poke_edit_data.personal[-1], poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension)) > 84):
+        silentremove(file_namer(poke_edit_data.personal_path, poke_edit_data.personal[-1], poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension)) 
     
     for offset in range(0, new_forme_count):
         #user will have specified which existing Forme to copy from
         
         #copy source personal file to new location
-        shutil.copy(file_namer(poke_edit_data.personal_path, personal_source_index, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.personal_path, start_location + offset + new_offset_start, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension))
-        personal_file_update(poke_edit_data, start_location + offset + new_offset_start, total_formes, start_location)
-        #print(start_location + offset + new_offset_start)
+        shutil.copy(file_namer(poke_edit_data.personal_path, personal_source_index, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.personal_path, start_location + offset, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension))
+        
+        #update new forme to have appropriate offset and forme count
+        personal_file_update(poke_edit_data, start_location + offset, total_formes, start_location)
+       
         #copy evolution file to new location
-        shutil.copy(file_namer(poke_edit_data.evolution_path, evolution_source_index, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.evolution_path, start_location + offset + new_offset_start, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension))
+        shutil.copy(file_namer(poke_edit_data.evolution_path, evolution_source_index, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.evolution_path, start_location + offset, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension))
         
         #copy levelup file to new location
-        shutil.copy(file_namer(poke_edit_data.levelup_path, levelup_source_index, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.levelup_path, start_location + offset + new_offset_start, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension))
-        
-        
+        shutil.copy(file_namer(poke_edit_data.levelup_path, levelup_source_index, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension), file_namer(poke_edit_data.levelup_path, start_location + offset, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension))
+
     
-    #rebuild personal compilation file
-    concatenate_bin_files(poke_edit_data.personal_path)
-    
-    
-    print("New data initialized")
+    print("Initializing new model data")
     
     #create new sets of model files
     #don't forget model index starts from 0 unlike everything else
@@ -227,14 +209,6 @@ def add_new_forme_execute(poke_edit_data, base_form_index, start_location, new_f
     
     #print('working indices, ', working_indices)
     
-    #First, if we moved existing alt formes, need to update their Personal Index
-    temp_offset = 0
-    for csv_index in working_indices:
-        if(poke_edit_data.master_list_csv[csv_index][3] in existing_formes_array):
-            poke_edit_data.master_list_csv[csv_index][3] = start_location + temp_offset
-            temp_offset += 1
-            #print(poke_edit_data.master_list_csv[csv_index])
-    
     #grab the base species name since we'll be using that at least once
     base_species_name = poke_edit_data.master_list_csv[working_indices[0]][0]
     
@@ -249,8 +223,7 @@ def add_new_forme_execute(poke_edit_data, base_form_index, start_location, new_f
     for offset in range(new_forme_count):
         #note that model index is set to zero, since we will do one big sweep after this to update all that come after, anyway
         #Forme name is set to the number alt forme it is (e.g. if we add a forme to a Pokemon with 3 existing alt formes, it will be 4 (as the base species itself is 0))
-        poke_edit_data.master_list_csv.insert(csv_insertion_point + offset, [base_species_name, new_offset_start + offset + 1, base_form_index, start_location + new_offset_start + offset, 0])
-    
+        poke_edit_data.master_list_csv.insert(csv_insertion_point + offset, [base_species_name, start_location + offset, base_form_index, start_location + offset, 0])
 
     #modelless_skip_count = 0
     #third we sweep through the entire array and update the model numbers, starting from the first newly inserted row
@@ -261,98 +234,27 @@ def add_new_forme_execute(poke_edit_data, base_form_index, start_location, new_f
         #        continue
         poke_edit_data.master_list_csv[offset][4] = offset - 1 #- modelless_skip_count
         
+
+    #finally, we sort the files so everything works nicely
+    poke_edit_data = resort_file_structure(poke_edit_data)
+        
     try:
         poke_edit_data = write_CSV(poke_edit_data)
     except:
         print('Please close your Pokemon Names and Files CSV if it is open')
-        poke_edit_data.csv_pokemon_list_path = askopenfilename(title='Select Pokemon Names and Files CSV')
+        poke_edit_data.csv_pokemon_list_path = asksaveasfile(title='Select Pokemon Names and Files CSV')
         write_CSV(poke_edit_data)
     print('Pokemon Names and Files CSV updated' + '\n')
         
     #poke_edit_data = load_names_from_CSV(poke_edit_data, True)
     
-    check_temp = poke_edit_data.master_list_csv.copy()
-    
     #refresh filenames
     poke_edit_data.run_model_later = False    
-    poke_edit_data = load_GARC(poke_edit_data, poke_edit_data.personal_path, "Personal", poke_edit_data.game, False)
+    poke_edit_data = load_GARC(poke_edit_data, poke_edit_data.personal_path, "Personal", poke_edit_data.game)
     poke_edit_data = load_GARC(poke_edit_data, poke_edit_data.levelup_path, "Levelup", poke_edit_data.game)
     poke_edit_data = load_GARC(poke_edit_data, poke_edit_data.evolution_path, "Evolution", poke_edit_data.game)
     poke_edit_data = load_GARC(poke_edit_data, poke_edit_data.model_path, "Model", poke_edit_data.game)
     print('Internal tables updated with changes' + '\n' + '\n')
                          
 
-    return(poke_edit_data)
-
-#sets some stuff up for forme insertion, final checks 
-def add_new_forme_prelim(poke_edit_data, base_form_index, new_forme_count, model_source_index, personal_source_index, levelup_source_index, evolution_source_index, def_model):
-
-    enough_room = False
-
-    #Grab any existing formes
-    existing_formes_array = []
-    with open(file_namer(poke_edit_data.personal_path, base_form_index, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension), "r+b") as f:
-        with mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE) as personal_hex_map:
-            #find existing formes
-            if(personal_hex_map[0x20] > 0x01):
-                for offset in range(0, personal_hex_map[0x20] - 1):
-                    existing_formes_array.append(personal_hex_map[0x1C] + personal_hex_map[0x1D]*256 + offset)
-                    
-    #find empty slot to write new forme(s) to
-    start_location = 0
-    
-    total_alt_formes = len(existing_formes_array) + new_forme_count
-    consecutive_open_found = 0
-    print('\n' + "Looking for first empty space to write to")
-    
-    for index in range(poke_edit_data.max_species_index, 9999):
-        path_temp = file_namer(poke_edit_data.personal_path, index, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension)
-        #if path exists, check if the file is zeroed out (because we moved some other forme somewhere else)
-        if(os.path.isfile(path_temp)):
-            #if the file is zero, increment consecutive counter by 1
-            if(file_is_zero(path_temp)):
-                
-                #if this is the first of this zero-block, tentatively grab it
-                if(consecutive_open_found == 0):
-                    start_location = index
-                
-                #increment counter
-                consecutive_open_found += 1
-                
-                #case there is enough room here to put everything
-                if(consecutive_open_found == total_alt_formes):
-                    break
-                #case there is enough cleared room after existing formes
-                elif(consecutive_open_found == total_alt_formes - len(existing_formes_array) and index - consecutive_open_found == existing_formes_array[-1]):
-                    start_location = existing_formes_array[0]
-                    enough_room = True
-                    #delete the zeroed out files to prepare:
-                    for x in range(0, consecutive_open_found):
-                        silentremove(file_namer(poke_edit_data.personal_path, existing_formes_array[-1] + x, poke_edit_data.personal_filename_length, poke_edit_data.extracted_extension))
-                        silentremove(file_namer(poke_edit_data.evolution_path, existing_formes_array[-1] + x, poke_edit_data.evolution_filename_length, poke_edit_data.extracted_extension))
-                        silentremove(file_namer(poke_edit_data.levelup_path, existing_formes_array[-1] + x, poke_edit_data.levelup_filename_length, poke_edit_data.extracted_extension))
-                
-            #otherwise check to see if the current file is big enough to be compilation file (we will overwrite it)
-            elif(os.path.getsize(path_temp) > 84):
-                start_location = index
-                break
-            #otherwise reset counter
-            else:
-                consecutive_open_found = 0
-        #specific edge case wherein there is room for the new forme(s) after existing the pre-existing formes of that Pokemon and that is the last existing forme index
-        elif(index - 1 == existing_formes_array[-1]):
-            start_location = existing_formes_array[0]
-            enough_room = True
-            break
-        #in this case, the compilation file does not exist, we are at the first empty slot and plant our flag here
-        else:
-            start_location = index
-            break
-    
-    if(len(existing_formes_array) == 0):
-        print('Existing formes moved, they now start at index ' + str(start_location))
-
-    print('Inserted new forme(s) starting at index ' + str(start_location + new_forme_count))
-    
-    poke_edit_data = add_new_forme_execute(poke_edit_data, base_form_index, start_location, new_forme_count, model_source_index, personal_source_index, levelup_source_index, evolution_source_index, existing_formes_array, def_model, total_alt_formes, enough_room)
     return(poke_edit_data)
